5fdae9ee7e476932ef79829106d7a650c635c556
Ronny Bergmann <git@ronnybergmann.net>
2023-02-18 20:47:11 +0100

    Fix a test.

--- test/test_riemannian_mean.jl
+++ test/test_riemannian_mean.jl
@@ -11,7 +11,7 @@ using Manifolds, Manopt, ManoptExamples, Test
     @test isapprox(M, p, grad_f(M, p), [0.0, 0.0])
     grad_f(M, X, p)
     @test isapprox(M, p, X, [0.0, 0.0])
-    o1 = ManoptExamples.Riemannian_mean_objective(data, X)
+    o1 = ManoptExamples.Riemannian_mean_objective(M, data; initial_vector=X)
     @test get_cost(M, o1, p) == f(M, p)
     @test get_gradient(M, o1, p) == grad_f(M, p)
     o2 = ManoptExamples.Riemannian_mean_objective(M, data)
